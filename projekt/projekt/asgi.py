64ed93a31c4125a082e4fe58abe5d8f27f884de0
zvzeq-swps <zbednarek@st.swps.edu.pl>
2025-01-22 19:14:13 +0100

    Django setup

--- projekt/projekt/asgi.py
+++ projekt/projekt/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for aplikacja project.
+ASGI config for projekt project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aplikacja.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projekt.settings')
 
 application = get_asgi_application()
--- projekt/projekt/wsgi.py
+++ projekt/projekt/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for aplikacja project.
+WSGI config for projekt project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aplikacja.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projekt.settings')
 
 application = get_wsgi_application()
